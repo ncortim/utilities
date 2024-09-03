5485956b109863262fefa538b31454dd507ea4c5
ncortim <ncortim@proton.me>
2024-09-03 17:25:44 +0200

    add documentation and set exit on error

--- rename.sh
+++ rename.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+#title           :rename-git-author.sh
+#description     :This scripts renames the author and email in all commits
+#                :in a git repository. Just place the correct values under
+#                :OLD_EMAIL, CORRECT_NAME, CORRECT_EMAIL. 
+#                :Your gitlab history will be then updated with the new names.
+#                :This script was taken from https://stackoverflow.com/questions/3042437/how-can-i-change-the-commit-author-for-a-single-commit
+#author          :ncortim
+#date            :03_09_2024
+#version         :0.1
+#usage           :bash rename-git-author.sh
+#                :./rename-git-author.sh
+#dependecies     :
+#==============================================================================
+
+#exit on error
+set -e
  
 git filter-branch --env-filter '
  
